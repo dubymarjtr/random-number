ca3302ea3ecd4adda5882984f7189b274b6acbf0
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-24 12:44:50 -0500

    feat: :sparkles: set font color

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const minimum = document.querySelector('#min');
 const maximum = document.querySelector('#max');
 const p = document.querySelector('p');
+const root = document.querySelector(':root');
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -15,3 +16,9 @@ document.querySelector('form').addEventListener('submit', event => {
   const maxValue = maximum.value;
   getRandomIntInclusive(minValue, maxValue);
 });
+
+document
+  .querySelector('input[type="color"]')
+  .addEventListener('input', event => {
+    root.style.setProperty('--font-color', `${event.target.value}`);
+  });
