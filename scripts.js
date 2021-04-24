1bd400d8378e93b44c1b03c33cd16c9966ec1846
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-24 12:32:59 -0500

    feat: :sparkles: display random number
    uses min and max values

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+const minimum = document.querySelector('#min');
+const maximum = document.querySelector('#max');
+const p = document.querySelector('p');
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const number = Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+  p.innerText = number;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+  const minValue = minimum.value;
+  const maxValue = maximum.value;
+  getRandomIntInclusive(minValue, maxValue);
+});
